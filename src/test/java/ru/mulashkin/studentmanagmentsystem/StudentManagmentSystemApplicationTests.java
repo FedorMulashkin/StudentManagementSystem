159cc941cfe298a72e8ecb53b8b9290c6172c74c
FedorMulashkin <fedor.mulashkin@gmail.com>
2022-10-17 12:40:00 +0300

    Add dependencies to project

---
+++ src/test/java/ru/mulashkin/studentmanagmentsystem/StudentManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mulashkin.studentmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
